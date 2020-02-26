e53faf7dc2d8473017472155d5073cbe109c86fd
Thiago Lanza <lanzathm@gmail.com>
2020-02-25 21:43:47 -0300

    Modules Exports para njk

---
+++ data.js
@@ -0,0 +1,42 @@
+//Exportação dos dados para o frontend
+module.exports = [
+    {
+        id: 'GykTLqODQuU',
+        title: 'Formulário Animado com JS puro e CSS Animation | Mayk Brito',
+        duration: '57 min',
+        price: 'free',
+        featured: true
+    },
+    {
+        id: '2alg7MQ6_sI',
+        title: 'Guia prático de Git e Github para iniciantes | Mayk Brito',
+        duration: '33 min',
+        price: 'free',
+        featured: true
+    },
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Entendendo como funciona o sistema de Grid CSS | Mayk Brito',
+        duration: '36 min',
+        price: 'free'
+    },
+    {
+        id: 'GSqR2i-Pq6o',
+        title: 'Desvendando o "this", como e quando usar | Mayk Brito',
+        duration: '48 min',
+        price: 'free'
+    },
+    {
+        id: 'ghTrp1x_1As',
+        title: 'O que é uma REST/RESTFUL API | Mayk Brito',
+        duration: '33 min',
+        price: 'free',
+        featured: true
+    },
+    {
+        id: 'rAzHvYnQ8DY',
+        title: 'Array: Funções de ordem superior | Mayk Brito',
+        duration: '54 min',
+        price: 'free'
+    }
+]
